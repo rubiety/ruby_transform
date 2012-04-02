3352837a2cf0c4ff6556f0fbe279a147e9caf81f
Ben Hughes <ben@railsgarden.com>
2012-04-01 17:43:49 -0700

    Explicitly requiring ruby_transform/transformer to minimize load order issues.

--- lib/ruby_transform.rb
+++ lib/ruby_transform.rb
@@ -3,6 +3,8 @@ require "active_support"
 require "ruby_parser"
 require "ruby_scribe"
 
+require "ruby_transform/transformer"
+
 Dir[File.join(File.dirname(__FILE__), "ruby_transform/**/**/*.rb")].each do |file|
   require file
 end
